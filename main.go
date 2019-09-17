a69b6b02cb8f58032d4f91f753cc171b922f48b0
omgj <olivermgjohnson@gmail.com>
2019-09-17 16:09:28 +1000

    cloudyml change

--- main.go
+++ main.go
@@ -11,12 +11,18 @@ import (
 
 func main() {
 	http.HandleFunc("/", public)
+	http.HandleFunc("/boss", boss)
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
 	}
 	log.Println("listening on 8080")
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
+}
+
+func boss(w http.ResponseWriter, r *http.Request) {
+	a, _ := ioutil.ReadFile("boss.html")
+	io.WriteString(w, string(a))
 }
 
 func public(w http.ResponseWriter, r *http.Request) {
