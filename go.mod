beccaf831aefde967e0b70f57f42525331d45281
omgj <olivermgjohnson@gmail.com>
2019-11-03 00:04:59 +1100

    testing

--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.13
 require (
 	cloud.google.com/go/firestore v1.0.0
 	github.com/google/uuid v1.1.1
+	github.com/stripe/stripe-go v66.1.1+incompatible
 )
--- main.go
+++ main.go
@@ -1,21 +1,24 @@
 package main
 
 import (
-	"io"
+	"context"
+	"encoding/json"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
-	"context"
-	"cloud.google.com/go/firestore"
-	"github.com/google/uuid"
+	"math/rand"
 	"net/http"
 	"net/url"
-	"strings"
 	"os"
-	"math/rand"
-	"encoding/json"
 	"strconv"
+	"strings"
 	"time"
+
+	"cloud.google.com/go/firestore"
+	"github.com/google/uuid"
+	stripe "github.com/stripe/stripe-go"
+	"github.com/stripe/stripe-go/customer"
 )
 
 var fs *firestore.Client
@@ -38,6 +41,7 @@ func main() {
 	http.HandleFunc("/whoami", whoami)
 	http.HandleFunc("/logout", logout)
 	http.HandleFunc("/txtpwd", txtpwd)
+	http.HandleFunc("/cardtoken", cardtoken)
 	http.HandleFunc("/codeconf", codeconf)
 	port := os.Getenv("PORT")
 	if port == "" {
@@ -47,13 +51,46 @@ func main() {
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
 }
 
+func whoam(r *http.Request) string {
+	c, e := r.Cookie("whart")
+	if e != nil {
+		return ""
+	}
+	ss := os.Getenv("SESHSECRET")
+	q, er := fs.Collection("sessions").Doc(c.Value + ss).Get(context.Background())
+	if er != nil {
+		return ""
+	}
+	a := q.Data()
+	u := a["user"].(string)
+	us := `0` + string(u[3:])
+	return us
+}
+
+func cardtoken(w http.ResponseWriter, r *http.Request) {
+	a := whoam(r)
+	if a == "" {
+		w.Write([]byte(`err`))
+		return
+	}
+	token := r.FormValue("token")
+	stripe.Key = "sk_test_XCvlmn17AXsURaJN66uYs1Mk"
+	cp := &stripe.CustomerParams{
+		Phone: stripe.String(a),
+	}
+	cp.SetSource(token)
+	c, _ := customer.New()
+	log.Print(c)
+	w.Write([]byte(`ok`))
+}
+
 func logout(w http.ResponseWriter, r *http.Request) {
 	c := &http.Cookie{
-		Name:     "whart",
-		Value:    "",
-		Path:     "/",
+		Name:    "whart",
+		Value:   "",
+		Path:    "/",
 		Expires: time.Unix(0, 0),
-	
+
 		HttpOnly: true,
 	}
 	http.SetCookie(w, c)
@@ -67,21 +104,21 @@ func whoami(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	sessionsecret := os.Getenv("SESHSECRET")
-	q, er := fs.Collection("sessions").Doc(c.Value+sessionsecret).Get(context.Background())
+	q, er := fs.Collection("sessions").Doc(c.Value + sessionsecret).Get(context.Background())
 	if er != nil {
 		w.Write([]byte(`none`))
 		return
 	}
 	mm := q.Data()
 	user := mm["user"].(string)
-	muser := "0"+user[3:]
+	muser := "0" + user[3:]
 	w.Write([]byte(muser))
 }
 
 func codeconf(w http.ResponseWriter, r *http.Request) {
 	code := r.FormValue("code")
 	number := r.FormValue("number")
-	num := "+61"+number[1:]
+	num := "+61" + number[1:]
 	ctx := context.Background()
 	people := fs.Collection("people")
 	who := people.Doc(num)
@@ -94,13 +131,13 @@ func codeconf(w http.ResponseWriter, r *http.Request) {
 	dm := ds.Data()
 	log.Print(dm)
 	if code == dm["code"].(string) {
-		if (int(time.Now().Unix())-int(dm["codevalidity"].(int64)))<30 {
+		if (int(time.Now().Unix()) - int(dm["codevalidity"].(int64))) < 30 {
 			uuids := uuid.New()
 			sessionsecret := os.Getenv("SESHSECRET")
-			uui := uuids.String()+sessionsecret
+			uui := uuids.String() + sessionsecret
 			_, errr := fs.Collection("sessions").Doc(uui).Set(context.Background(), map[string]interface{}{
 				"sessioncreated": int(time.Now().Unix()),
-				"user": num,
+				"user":           num,
 			})
 			if errr != nil {
 				w.Write([]byte(`err`))
@@ -108,7 +145,7 @@ func codeconf(w http.ResponseWriter, r *http.Request) {
 			}
 			coo := http.Cookie{Name: "whart", Value: uuids.String(), Path: "/"}
 			http.SetCookie(w, &coo)
-			w.Write([]byte("0"+num[3:]))
+			w.Write([]byte("0" + num[3:]))
 			return
 		}
 	}
@@ -116,15 +153,15 @@ func codeconf(w http.ResponseWriter, r *http.Request) {
 }
 
 func txtpwd(w http.ResponseWriter, r *http.Request) {
-	num := "+61"+r.FormValue("numuser")[1:]
+	num := "+61" + r.FormValue("numuser")[1:]
 	log.Println("confirming: ", num)
 	_, er := fs.Collection("people").Doc(num).Get(context.Background())
 	code := sendSms(num)
 	if er != nil {
 		_, e := fs.Collection("people").Doc(num).Set(context.Background(), map[string]interface{}{
 			"codevalidity": int(time.Now().Unix()),
-			"code": code,
-			"role": "customer",
+			"code":         code,
+			"role":         "customer",
 		})
 		if e != nil {
 			w.Write([]byte(`err`))
@@ -151,30 +188,30 @@ func sendSms(to string) string {
 	randomnumber := strconv.Itoa(r.Intn(1000000))
 	msgData := url.Values{}
 	msgData.Set("To", to)
-	msgData.Set("From",twilioNumber)
+	msgData.Set("From", twilioNumber)
 	msgData.Set("Body", randomnumber)
 	msgDataReader := *strings.NewReader(msgData.Encode())
 	client := &http.Client{}
 	twilioSid := os.Getenv("TWILSID")
-	twilioUrl := "https://api.twilio.com/2010-04-01/Accounts/"+twilioSid+"/Messages.json"
+	twilioUrl := "https://api.twilio.com/2010-04-01/Accounts/" + twilioSid + "/Messages.json"
 	req, _ := http.NewRequest("POST", twilioUrl, &msgDataReader)
 	twilioAuth := os.Getenv("TWILAUTH")
 	req.SetBasicAuth(twilioSid, twilioAuth)
 	req.Header.Add("Accept", "application/json")
 	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
 	resp, _ := client.Do(req)
-	if (resp.StatusCode >= 200 && resp.StatusCode < 300) {
+	if resp.StatusCode >= 200 && resp.StatusCode < 300 {
 		var data map[string]interface{}
 		decoder := json.NewDecoder(resp.Body)
 		err := decoder.Decode(&data)
-		if (err == nil) {
+		if err == nil {
 			log.Println(data["sid"])
 			return randomnumber
 		}
-		} else {
-			log.Println(resp.Status);
-			return ""
-		}
+	} else {
+		log.Println(resp.Status)
+		return ""
+	}
 	return ""
 }
 
